3f096d57b8b58e14d5d1f03ee41aef0b21b557d2
Leekyoungwook <lkw001110code@gmail.com>
2025-01-23 17:38:22 +0900

    fixed

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,14 +4,15 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between bg-gray-100 p-4">
-      <div className="flex items-center">
-        <img src={AnifarmLogo} alt="Logo" className="h-8 w-8 mr-2" />
-      </div>
+    <div className="flex items-center justify-between bg-gray-100 py-4">
+      <Link to="/" className="flex-shrink-0 flex items-center">
+        <img
+          src={AnifarmLogo}
+          alt="Logo"
+          className="h-8 w-8 md:h-12 md:w-12 mr-1 rounded-lg"
+        />
+      </Link>
       <div className="flex space-x-4">
-        <Link to="/" className="text-black">
-          홈페이지
-        </Link>
         <Link to="/login" className="text-black">
           로그인
         </Link>
