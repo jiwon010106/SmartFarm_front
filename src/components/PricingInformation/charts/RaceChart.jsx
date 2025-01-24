dd58bf04564811d4d42a8b8ae2a9e5fdb7d427a8
jiwon010106 <jjiwon753@gmail.com>
2025-01-24 14:31:38 +0900

    test3

--- src/components/PricingInformation/charts/RaceChart.jsx
+++ src/components/PricingInformation/charts/RaceChart.jsx
@@ -1,30 +1,7 @@
-<<<<<<< HEAD:src/components/PricingInformation/charts/RaceChart.jsx
-import React, { useEffect } from "react";
-import { createMarketChart } from "../../data/marketChart";
-
-const RaceChart = () => {
-  useEffect(() => {
-    const chart = createMarketChart("chartdiv");
-
-    return () => {
-      chart.dispose();
-    };
-  }, []);
-
-  return (
-    <div
-      id="chartdiv"
-      className="w-full h-[500px] mt-5 mb-10 border-2 border-gray-300 rounded-lg"
-    ></div>
-  );
-};
-
-export default RaceChart;
-=======
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchGetMarketData } from "../../redux/slices/apiSlice";
-import { createMarketChart } from "../../data/marketChart";
+import { fetchGetMarketData } from "../../../redux/slices/apiSlice";
+import { createMarketChart } from "../../../data/marketChart";
 
 const RaceChart = () => {
   const dispatch = useDispatch();
@@ -53,4 +30,3 @@ const RaceChart = () => {
 };
 
 export default RaceChart;
->>>>>>> 307122d4f498bdc85e0c40204da4e1a3484db94a:src/components/charts/RaceChart.jsx
