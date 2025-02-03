e047b69a33559e6ce9202a21bb9fa6f183c7f723
Leekyoungwook <lkw001110code@gmail.com>
2025-02-03 16:51:02 +0900

    main delect

--- src/components/common/Home.jsx
+++ src/components/common/Home.jsx
@@ -42,44 +42,6 @@ const Home = () => {
       <div className="w-[500px] h-[420px] text-white rounded-lg p-4">
         <div id="top5-chart" ref={chartRef} />
       </div>
-      <nav className="overflow-hidden w-full h-screen flex">
-        <ol className="flex items-center w-full">
-          {["Home", "About", "Widgets", "Kabobs", "Contact"].map(
-            (item, index) => (
-              <li
-                key={index}
-                className={`h-20 w-full flex items-center justify-center cursor-pointer transition-all duration-400 relative ${
-                  index === 2 ? "bg-blue-500 text-white" : ""
-                }`}
-                tabIndex={index + 1}
-              >
-                <a
-                  href="#0"
-                  className="text-2xl font-bold transition-all duration-200"
-                >
-                  {item}
-                </a>
-                {index === 2 && (
-                  <ol className="absolute top-6 left-0 text-center hidden group-hover:block">
-                    {["Big Widgets", "Bigger Widgets", "Huge Widgets"].map(
-                      (subItem, subIndex) => (
-                        <li
-                          key={subIndex}
-                          className="transition-transform duration-300"
-                        >
-                          <a href="#0" className="text-xs">
-                            {subItem}
-                          </a>
-                        </li>
-                      )
-                    )}
-                  </ol>
-                )}
-              </li>
-            )
-          )}
-        </ol>
-      </nav>
     </div>
   );
 };
