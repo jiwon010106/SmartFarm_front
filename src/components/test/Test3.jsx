df90a1cf446dafc5e5a6fd27e48ca2e9e3403f73
noseunghyeon <ro981009@gmail.com>
2025-02-04 17:45:36 +0900

    green

--- src/components/test/Test3.jsx
+++ src/components/test/Test3.jsx
@@ -84,7 +84,7 @@ const Test3 = () => {
   }, [activeIndex]);
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-600 to-purple-400 flex items-center justify-center select-none">
+    <div className="min-h-screen bg-gradient-to-br from-green-400 to-green-200 flex items-center justify-center select-none">
       <div className="text-center whitespace-nowrap overflow-hidden">
         {images.map((image, index) => (
           <div
