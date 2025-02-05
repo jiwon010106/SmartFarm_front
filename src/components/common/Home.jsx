aeb807db09cbe3a25eaf1167078a8fd2c1567c96
Leekyoungwook <lkw001110code@gmail.com>
2025-02-05 09:59:31 +0900

    ?

--- src/components/common/Home.jsx
+++ src/components/common/Home.jsx
@@ -37,12 +37,40 @@ const Home = () => {
   }
 
   return (
-    <div className="container">
-      <img src={landingImg} alt="" className="w-full" />
-      <h1> Welcome to the Home Page </h1>
-      <Link to="/pricingInformation">Go to Pricing Information</Link>
-      <div className="w-[500px] h-[420px] text-white rounded-lg p-4">
-        <div id="top5-chart" ref={chartRef} />
+    <div className="w-full">
+      <div className="relative">
+        <div className="overflow-hidden flex justify-center items-center relative max-h-[600px] min-h-[300px]">
+          <div className="absolute opacity-30 overlay w-full h-full bg-white left-0 top-0"></div>
+          <div
+            className="slogan-box absolute 
+          left-1/2 transform -translate-x-1/2 
+          lg:left-[20%] lg:transform-none 
+          top-[25%] flex flex-col gap-4"
+          >
+            <h2
+              style={{ fontFamily: "LemonMilk" }}
+              className="text-6xl hidden lg:block"
+            >
+              AnIfarm
+            </h2>
+            <p className="text-base lg:text-lg tracking-tight hidden lg:block">
+              사이트
+              <br />
+              소개
+              <br />글 입니다.
+            </p>
+          </div>
+          <img
+            src={landingImg}
+            className="w-full max-h-[600px] min-h-[300px]"
+            alt=""
+          />
+          <h1> Welcome to the Home Page </h1>
+          <Link to="/pricingInformation">Go to Pricing Information</Link>
+          <div className="w-[500px] h-[420px] text-white rounded-lg p-4">
+            <div id="top5-chart" ref={chartRef} />
+          </div>
+        </div>
       </div>
     </div>
   );
